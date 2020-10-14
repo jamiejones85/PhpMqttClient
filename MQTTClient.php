181b63b7083178ec6e63420312bdd631391be3af
Jamie Jones <jamie@mudslidedesign.co.uk>
2020-10-14 15:20:38 +0100

    Adding constructor param to allow clientIds > 23 characters in length

--- MQTTClient.php
+++ MQTTClient.php
@@ -219,17 +219,17 @@ class MQTTClient {
 	 * @param integer $timeout Number of millliseconds before timeout when reading from socket
 	 * @return boolean Returns false if connection failed
 	 */
-	public function sendConnect($clientId, $cleanSession=false, $keepAlive=10, $timeout=5000) {
+	public function sendConnect($clientId, $cleanSession=false, $keepAlive=10, $timeout=5000, $strictClientId=true) {
 	    if (!$this->serverAddress) return false;
 
 	    // Basic validation of clientid
 	    // Note: A MQTT server may accept other chars and more than 23 chars in the clientid but that is optional, 
 	    // all chars below up to 23 chars are required to be accepted (see section "3.1.3.1 Client Identifier" of the standard)
-	    if(preg_match("/[^0-9a-zA-Z]/",$clientId)) {
+	    if($strictClientId && preg_match("/[^0-9a-zA-Z]/",$clientId)) {
 	        $this->debugMessage('ClientId can only contain characters 0-9,a-z,A-Z');
 	        return false;
 	    }
-	    if(strlen($clientId) > 23) {
+	    if($strictClientId && strlen($clientId) > 23) {
 	        $this->debugMessage('ClientId max length is 23 characters/numbers');
 	        return false;
 	    }
